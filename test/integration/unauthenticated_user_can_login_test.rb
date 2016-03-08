b35b95f5748ddcae77b8f78614c1e5c24276f397
Tyler Komoroske <tjkomor@clemson.edu>
2016-03-08 11:27:51 -0700

    add test to assert user cannot login with incorrect email

--- test/integration/unauthenticated_user_can_login_test.rb
+++ test/integration/unauthenticated_user_can_login_test.rb
@@ -14,4 +14,15 @@ class UnauthenticatedUserCanLoginTest < ActionDispatch::IntegrationTest
 
    assert_equal task_lists_path, current_path
  end
+
+ test "unauthenticated user is redirected to login page after entering an invalid email" do
+   user = User.create(email: "tyler@example.com", password: "password")
+   visit "/login"
+
+   fill_in "Email", with: "yooooo"
+   fill_in "Password", with: "password"
+   click_button "Login"
+
+   assert_equal "/login", current_path
+ end
 end
