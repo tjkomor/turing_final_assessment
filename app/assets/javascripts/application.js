7eb6cbc1a1d8fbc962529d6bf2e105d7c8239bca
Tyler Komoroske <tjkomor@clemson.edu>
2016-03-08 16:38:25 -0700

    user can sort articles alphabetically user can sort articles by read status

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,64 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function () {
+  readStatus();
+  sortTitle();
+  sortReadStatus();
+});
+
+var readStatus = function () {
+  $('tr').delegate('#read', 'click', function() {
+    $link = $(this).closest('tr');
+    toggleReadStatus($link.data('id'));
+    $link.toggleClass('true');
+    if ($link.hasClass('true')) {
+      $(this).text('Yes');
+    } else {
+      $(this).text('No');
+    }
+  });
+};
+
+var toggleReadStatus = function (id) {
+  $.ajax({
+    type: 'PATCH',
+    url: '/api/v1/links/' + id,
+    success: function() {
+      console.log("success");
+    }
+  });
+};
+
+var sortTitle = function () {
+  $('.sortTitle').on('click', function() {
+    var $links = $('.link');
+    alphabeticalSort = $links.sort(function (first, second) {
+      first = $(first).data('title').toLowerCase();
+      second = $(second).data('title').toLowerCase();
+      if (first < second) return -1;
+      if (first > second) return 1;
+      return 0;
+    });
+    $links.remove();
+    $('.table').append(alphabeticalSort);
+    readStatus();
+  });
+};
+
+var sortReadStatus = function () {
+  $('.sortReadStatus').on('click', function() {
+    var $links = $('.link');
+    sortedLinks = $links.sort(function (first, second) {
+      first = $(first).hasClass('true');
+      second = $(second).hasClass('true');
+      if (first < second) return -1;
+      if (first > second) return 1;
+      return 0;
+    });
+    $links.remove();
+    $('.table').append(sortedLinks);
+    readStatus();
+  });
+};
