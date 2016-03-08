457c3d31172b79a43d08bacd624bb918773ab971
Tyler Komoroske <tjkomor@clemson.edu>
2016-03-08 10:45:21 -0700

    create test for unique user email

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308172644) do
+ActiveRecord::Schema.define(version: 20160308173903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "email must be unique" do
+    user1 = User.create(email: 'tyler@tyler.com', password: 'password')
+    user2 = User.new(email: 'tyler@tyler.com', password: 'password')
+    assert user1.valid?
+    refute user2.valid?
+  end
 end
