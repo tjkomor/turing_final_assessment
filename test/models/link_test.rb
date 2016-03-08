a59da5b2e71ce2e9f65c31cc6a61eafef6ab7f25
Tyler Komoroske <tjkomor@clemson.edu>
2016-03-08 13:09:49 -0700

    user must submit valid url

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class TaskListTest < ActiveSupport::TestCase
-  test "link is invalid without title" do
+  test "link is invalid without valid url or title" do
     link = Link.new(url: "Testing validation")
-    valid_link = Link.new(title: 'cool', url: "ballin")
+    valid_link = Link.new(title: 'cool', url: "http://espn.go.com")
 
     refute link.valid?
     assert valid_link.valid?
